069ab3b5034a705b15be4ed3e7d6427560f3f165
Alexbnet <alexbnet@hotmail.co.uk>
2022-02-12 00:00:30 +0000

    Problem #1 - Success!

---
+++ Problem #1 - Sum Checker/sum_checker.js
@@ -0,0 +1,19 @@
+function sum_checker (list, target) {
+    for (let i = 0; i < list.length; i++) {
+        let addedArray = []
+        for (let j = 0; j < i; j++) {
+            addedArray[j] = list[j] + list[i]
+            if (addedArray[j] == target) {
+                //return String(list[i]).concat(' and ', String(list[j]), ' sum to ', String(target), '!')
+                return true
+            }
+        }
+    }
+    //return 'Nothing sums to '.concat(target, '... :(')
+    return false
+}
+
+let list = [10, 15, 3, 7]
+let target = 17
+
+console.log(sum_checker(list, target))
