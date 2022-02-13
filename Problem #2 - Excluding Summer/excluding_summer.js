d81c6a788a414f4657f273c542a168b0ea9787d7
Alexbnet <alexbnet@hotmail.co.uk>
2022-02-13 10:48:06 +0000

    Problem #2 - Dominant Performance Vs. Hard

---
+++ Problem #2 - Excluding Summer/excluding_summer.js
@@ -0,0 +1,20 @@
+function excluding_summer (list) {
+    let outputList = []
+    for (let i = 0; i < list.length; i++) {
+        outputList[i] = 1
+        for (let j = 0; j < list.length; j++) {
+            if (j != i) {
+                outputList[i] *= list[j]
+            }
+        }
+    }
+    return outputList
+}
+
+let list = [1, 2, 3, 4, 5]
+
+console.log(excluding_summer(list))
+
+list = [3, 2, 1]
+
+console.log(excluding_summer(list))
