37104225096a0fbdd70795cd04211dfe9367f811
Alexbnet <alexbnet@hotmail.co.uk>
2022-02-27 15:00:19 +0000

    Almost caught up!

---
+++ Problem #12 - Staircase Climber/staircase_climber.py
@@ -0,0 +1,12 @@
+def staircase(n):
+    a, b = 1, 2
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return a
+
+def variable_staircase(n, X):
+    cache = [0 for _ in range(n + 1)]
+    cache[0] = 1
+    for i in range(1, n + 1):
+        cache[i] += sum(cache[i - x] for x in X if i - x >= 0)
+    return cache[n]
