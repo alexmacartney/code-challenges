bc6b4cad45f8a323836080be9098a2894125b245
Alexbnet <alexbnet@hotmail.co.uk>
2022-04-18 01:44:48 +0100

    Final edit...

--- Problem #61 - Integer Exponentiation/integer_exponentiation.py
+++ Problem #61 - Integer Exponentiation/integer_exponentiation.py
@@ -1,11 +1,28 @@
-def power(x, y):
+def power_a(x, y):
+    base = x
+    exponent = y
     if y < 0:
-        return power(1 / x, -y)
+        base = 1 / x
+        exponent = -y
+    coeff = 1
+    while y > 1:
+        if y % 2 == 0:
+            base *= base
+            y = y // 2
+        else:
+            coeff *= base
+            base *= base
+            y = (y - 1) // 2
+    return coeff * base
+
+def power_b(x, y):
+    if y < 0:
+        return power_b(1 / x, -y)
     elif y == 0:
         return 1
     elif y == 1:
         return x
     elif y % 2 == 0:
-        return power(x * x, y // 2)
+        return power_b(x * x, y // 2)
     else: # y is odd
-        return x * power(x * x, y // 2)
+        return x * power_b(x * x, y // 2)
